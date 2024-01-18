d68e9dba4f191120c7875de9ac17358074171746
luisferlie <luisferlie@gmail.com>
2024-01-18 13:59:21 +0100

    modif conf

--- script.js
+++ script.js
@@ -2,18 +2,31 @@ const modal = document.querySelector('.modal')
 const overlay = document.querySelector('.overlay')
 const btnCloseModal = document.querySelector('.close-modal')
 const btnsOpenModal = document.querySelectorAll('.show-modal')
+
 console.log(btnsOpenModal)
+
 const openModal = function () {
-  modal.classList.remove('hidden')
-  overlay.classList.remove('hidden')
+    modal.classList.remove('hidden')
+    overlay.classList.remove('hidden')
 }
 
 const closeModal = function () {
-  modal.classList.add('hidden')
-  overlay.classList.add('hidden')
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden')
 }
-
+btnsOpenModal.forEach((element) => {
+    element.addEventListener('click', openModal)
+})
+/*
 for (let i = 0; i < btnsOpenModal.length; i++)
   btnsOpenModal[i].addEventListener('click', openModal)
+*/
 
 btnCloseModal.addEventListener('click', closeModal)
+
+document.addEventListener('keydown', function (e) {
+    console.log(e, e.key)
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal()
+    }
+})
